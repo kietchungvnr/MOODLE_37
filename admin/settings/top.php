a14758e163d897140a3e961e208588b297f4edb5
lethanhvuvnr <vu.lethanh@vnresource.net>
2020-08-15 11:40:37 +0700

    Ẩn các field và module không cần thiết - IVC

--- admin/settings/top.php
+++ admin/settings/top.php
@@ -28,20 +28,20 @@ if ($hassiteconfig) {
 $ADMIN->add('root', new admin_category('users', new lang_string('users','admin')));
 $ADMIN->add('root', new admin_category('courses', new lang_string('courses','admin')));
 $ADMIN->add('root', new admin_category('grades', new lang_string('grades')));
-$ADMIN->add('root', new admin_category('analytics', new lang_string('analytics', 'analytics')));
+$ADMIN->add('root', new admin_category('analytics', new lang_string('analytics', 'analytics'), true));
 $ADMIN->add('root', new admin_category('competencies', new lang_string('competencies', 'core_competency')));
 $ADMIN->add('root', new admin_category('badges', new lang_string('badges'), empty($CFG->enablebadges)));
 $ADMIN->add('root', new admin_category('location', new lang_string('location','admin')));
 $ADMIN->add('root', new admin_category('language', new lang_string('language')));
 $ADMIN->add('root', new admin_category('messaging', new lang_string('messagingcategory', 'admin')));
-$ADMIN->add('root', new admin_category('modules', new lang_string('plugins', 'admin')));
+$ADMIN->add('root', new admin_category('modules', new lang_string('plugins', 'admin'), true));
 $ADMIN->add('root', new admin_category('security', new lang_string('security','admin')));
 $ADMIN->add('root', new admin_category('appearance', new lang_string('appearance','admin')));
 $ADMIN->add('root', new admin_category('frontpage', new lang_string('frontpage','admin')));
 $ADMIN->add('root', new admin_category('server', new lang_string('server','admin')));
 $ADMIN->add('root', new admin_category('mnet', new lang_string('net','mnet'), (isset($CFG->mnet_dispatcher_mode) and $CFG->mnet_dispatcher_mode === 'off')));
 $ADMIN->add('root', new admin_category('reports', new lang_string('reports')));
-$ADMIN->add('root', new admin_category('development', new lang_string('development', 'admin')));
+$ADMIN->add('root', new admin_category('development', new lang_string('development', 'admin'), true));
 $ADMIN->add('root', new admin_category('newsvnr', new lang_string('sitetabadmin', 'local_newsvnr')));
 
 // hidden unsupported category
