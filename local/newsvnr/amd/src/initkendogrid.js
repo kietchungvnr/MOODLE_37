214ac8f47967928b2a992b554c1ab2bff3ba6ea4
lethanhvuvnr <vu.lethanh@vnresource.net>
2020-11-09 09:49:35 +0700

    Upbuild 07112020

--- local/newsvnr/amd/src/initkendogrid.js
+++ local/newsvnr/amd/src/initkendogrid.js
@@ -1,143 +1,181 @@
 define(['jquery', 'core/config', 'core/str','kendo.all.min'], function($, Config, Str, kendo) {
 	var initGrid = function (gridConfig) {
-            debugger
-            if (gridConfig.columns === undefined) { gridConfig.columns = []; }
+        if (gridConfig.columns === undefined) { gridConfig.columns = []; }
 
-            gridConfig.selectData = function (e) {
-                var myGrid = $('div[kendo-grid][k-options=' + gridConfig.gridName + ']').getKendoGrid();
-                var selectedRows = myGrid.select();
-                var arrObject = [];
-                for (var i = 0; i < selectedRows.length; i++) {
-                    arrObject.push(myGrid.dataItem(selectedRows[i]));
-                }
-                return arrObject;
-            };
-            gridConfig.selectId = function (e) {
-                var myGrid = $('div[kendo-grid][k-options=' + gridConfig.gridName + ']').getKendoGrid();
-                var selectedRows = myGrid.select();
-                var arrObject = [];
-                for (var i = 0; i < selectedRows.length; i++) {
-                    arrObject.push(myGrid.dataItem(selectedRows[i]).Id);
-                }
-                return arrObject;
-            };
-            gridConfig.columns.unshift({
-                selectable: true, width: 55
-            });
-            //edit
-            if (gridConfig.approvalModuleEvent != undefined) {
-                var funcApprovalModule = function(e) {
-                    e.preventDefault();
-                    var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
-                    var script = Config.wwwroot + '/local/newsvnr/ajax/library_online/library_approval_module_ajax.php?action=approval';
-                    var settings = {
-                        type:"POST",
-                        processData:true,
-                        data:{
-                            moduleid:dataItem.id
-                        }
-                    }
-                    $.ajax(script,settings).then(function() {
-                    }) 
-                    gridConfig.approvalModuleEvent(dataItem);
-                }
-                var objEventApprovalModule = {
-                    click: funcApprovalModule,
-                    text: " ",
-                    name: "approval",
-                    iconClass: 'fa fa-check',
-                }
-                eventArr.push(objEventApprovalModule);
+        gridConfig.selectData = function (e) {
+            var myGrid = $('div[kendo-grid][k-options=' + gridConfig.gridName + ']').getKendoGrid();
+            var selectedRows = myGrid.select();
+            var arrObject = [];
+            for (var i = 0; i < selectedRows.length; i++) {
+                arrObject.push(myGrid.dataItem(selectedRows[i]));
             }
-            if (gridConfig.deleteModuleEvent != undefined) {
-                var funcDeleteModule = function(e) {
-                    e.preventDefault();
-                    var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
-                    var script = Config.wwwroot + '/local/newsvnr/ajax/library_online/library_approval_module_ajax.php?action=delete';
-                    var settings = {
-                        type:"POST",
-                        processData:true,
-                        data:{
-                            moduleid:dataItem.id
-                        }
-                    }
-                    $.ajax(script,settings).then(function() {
-                    }) 
-                    gridConfig.deleteModuleEvent(dataItem);
-                }
-                var objEventDeleteModule = {
-                    click: funcDeleteModule,
-                    text: " ",
-                    name: "delete",
-                    iconClass: 'fa fa-trash',
-                }
-                eventArr.push(objEventDeleteModule);
-            }
-            if (gridConfig.editEvent != undefined) {
-                var funcEdit = function (e) {
-                    e.preventDefault();
-                    var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
-                    gridConfig.editEvent(dataItem);
-                }
-                gridConfig.columns.push({
-                    title: 'Edit',
-                    command: {
-                        click: funcEdit, text: " ", name: "edit", iconClass: 'fa fa-pencil-square-o'
-                    },
-                    width: 80
-                });
-            }
-
-            if (gridConfig.deleteEvent != undefined) {
-                var funcDelete = function (e) {
-                    e.preventDefault();
-                    var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
-                    gridConfig.deleteEvent(dataItem);
-                }
-                gridConfig.columns.push({
-                    title: 'Delete',
-                    command: {
-                        click: funcDelete, text: " ", name: "delete", iconClass: 'fa-trash'
-                    },
-                    width: 80
-                });
-            }
-
-            debugger
-            var kendoGrid = {
-            	dataSource: newDatasourceGrid(gridConfig),
-                persistSelection: true,
-                groupable: false,
-                //sortable: true,
-                resizable: true,
-                //dataBound: gridConfig.dataBound,
-                //height: 520,
-                pageable: {
-                    refresh: true,
-                    pageSizes: true,
-                    pageSizes: [10, 20, 50, 100],
-                    buttonCount: 5
-                },
-                columns: gridConfig.columns,
-                noRecords: {
-                    template: 'No Records'
-                }
-            }
-            return kendoGrid;
+            return arrObject;
         };
-
-    var newDatasourceGrid = function (gridConfig) {
-        var dataSource = new kendo.data.DataSource({
+        gridConfig.selectId = function (e) {
+            var myGrid = $('div[kendo-grid][k-options=' + gridConfig.gridName + ']').getKendoGrid();
+            var selectedRows = myGrid.select();
+            var arrObject = [];
+            for (var i = 0; i < selectedRows.length; i++) {
+                arrObject.push(myGrid.dataItem(selectedRows[i]).Id);
+            }
+            return arrObject;
+        };
+        gridConfig.columns.unshift({
+            selectable: true, width: 55
+        });
+        //edit
+        if (gridConfig.approvalModuleEvent != undefined) {
+            var funcApprovalModule = function(e) {
+                e.preventDefault();
+                var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                var script = Config.wwwroot + '/local/newsvnr/ajax/library_online/library_approval_module_ajax.php?action=approval';
+                var settings = {
+                    type:"POST",
+                    processData:true,
+                    data:{
+                        moduleid:dataItem.id
+                    }
+                }
+                $.ajax(script,settings).then(function() {
+                }) 
+                gridConfig.approvalModuleEvent(dataItem);
+            }
+            var objEventApprovalModule = {
+                click: funcApprovalModule,
+                text: " ",
+                name: "approval",
+                iconClass: 'fa fa-check',
+            }
+            eventArr.push(objEventApprovalModule);
+        }
+        if (gridConfig.deleteModuleEvent != undefined) {
+            var funcDeleteModule = function(e) {
+                e.preventDefault();
+                var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                var script = Config.wwwroot + '/local/newsvnr/ajax/library_online/library_approval_module_ajax.php?action=delete';
+                var settings = {
+                    type:"POST",
+                    processData:true,
+                    data:{
+                        moduleid:dataItem.id
+                    }
+                }
+                $.ajax(script,settings).then(function() {
+                }) 
+                gridConfig.deleteModuleEvent(dataItem);
+            }
+            var objEventDeleteModule = {
+                click: funcDeleteModule,
+                text: " ",
+                name: "delete",
+                iconClass: 'fa fa-trash',
+            }
+            eventArr.push(objEventDeleteModule);
+        }
+        if (gridConfig.editEvent != undefined) {
+            var funcEdit = function (e) {
+                e.preventDefault();
+                var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                gridConfig.editEvent(dataItem);
+            }
+            $.ajax(script,settings).then(function() {
+            }) 
+            gridConfig.approvalModuleEvent(dataItem);
+        }
+        var objEventApprovalModule = {
+            click: funcApprovalModule,
+            text: " ",
+            name: "approval",
+            iconClass: 'fa fa-check',
+        }
+        eventArr.push(objEventApprovalModule);
+        
+        if (gridConfig.deleteModuleEvent != undefined) {
+            var funcDeleteModule = function(e) {
+                e.preventDefault();
+                var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                var script = Config.wwwroot + '/local/newsvnr/ajax/library_online/library_approval_module_ajax.php?action=delete';
+                var settings = {
+                    type:"POST",
+                    processData:true,
+                    data:{
+                        moduleid:dataItem.id
+                    }
+                }
+                $.ajax(script,settings).then(function() {
+                }) 
+                gridConfig.deleteModuleEvent(dataItem);
+            }
+            var objEventDeleteModule = {
+                click: funcDeleteModule,
+                text: " ",
+                name: "delete",
+                iconClass: 'fa fa-trash',
+            }
+            eventArr.push(objEventDeleteModule);
+        }
+        if (gridConfig.editEvent != undefined) {
+            var funcEdit = function(e) {
+                e.preventDefault();
+                var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                gridConfig.editEvent(dataItem);
+            }
+            var objEventEdit = {
+                click: funcEdit,
+                text: " ",
+                name: "edit",
+                iconClass: 'fa fa-pencil-square-o',
+            }
+            eventArr.push(objEventEdit);
+        }
+        if (gridConfig.deleteEvent != undefined) {
+            var funcDelete = function(e) {
+                e.preventDefault();
+                var dataItem = this.dataItem($(e.currentTarget).closest("tr"));
+                gridConfig.deleteEvent(dataItem);
+            }
+            var objEventDelete = {
+                click: funcDelete,
+                text: " ",
+                name: "delete",
+                iconClass: 'fa fa-trash-o'
+            }
+            eventArr.push(objEventDelete);
+        }
+        gridConfig.columns.push({
+            title: 'Action',
+            command: eventArr,
+            width: 100
+        });
+        return {
+            dataSource: newDatasourceGrid(gridConfig),
+            persistSelection: true,
+            groupable: false,
+            //sortable: true,
+            resizable: true,
+            //dataBound: gridConfig.dataBound,
+            //height: 520,
+            toolbar: ["search"],
+            search: {
+                fields: ["name"]
+            },
+            pageable: {
+                refresh: true,
+                pageSizes: true,
+                pageSizes: [10, 20, 50, 100],
+                buttonCount: 5
+            },
+            columns: gridConfig.columns,
+            noRecords: {
+                template: 'No Records'
+            },
+            // editable: "popup"
+        }
+    };
+    var newDatasourceGrid = function(gridConfig) {
+        return new kendo.data.DataSource({
             transport: {
-                read: {
-                	url: Config.wwwroot + '/local/newsvnr/exam/ajax/exam.php',
-					dataType: "json",
-                    contentType: 'application/json; charset=utf-8',
-                    type: "GET",
-					data : {
-						action : 'subjectexam_grid'
-					}
-                },
+                read: gridConfig.apiSettings,
                 parameterMap: function(options, operation) {
                     if (operation == "read") {
                         if (options["filter"]) {
@@ -157,25 +195,21 @@ define(['jquery', 'core/config', 'core/str','kendo.all.min'], function($, Config
             serverFiltering: true,
             serverSorting: true,
             schema: {
-                id: "code",
-                fields: {
-                    name: {
-                        type: "string"
-                    },
-                    code: {
-                        type: "string"
-                    },
+                model: {
+                    id : "id",
+                    fields: {
+                        id : { type: "number" },
+                        name: { type: "string" },
+                    }
                 },
                 total: function(data) {
-                    if(data != null && data.length > 0)
-                        return data[0].total; 
+                    if (data != null && data.length > 0) return data[0].total;
                 },
             }
         });
-  
-        return dataSource;
     };
 	return {
 		initGrid: initGrid
 	}
 });
+
