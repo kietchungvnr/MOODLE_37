25dc6a666d16038c64b156f35fbde266e20de5e3
lethanhvuvnr <vu.lethanh@vnresource.net>
2021-03-06 12:10:08 +0700

    Chức năng xem phân tích câu hỏi MCQ

---
+++ mod/quiz/report/mcq/lang/vi/quiz_mcq.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'quiz_grading', language 'en', branch 'MOODLE_22_STABLE'
+ *
+ * @package    quiz
+ * @subpackage mcq
+ * @copyright  2012 Steve Bond
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['allstudents'] = 'Tất cả học viên trong khóa';
+$string['allusers'] = 'Tất cả học viên trong hệ thống';
+$string['correctoption'] = 'Đáp án đúng';
+$string['correctoptions'] = 'Đáp án đúng';
+$string['filter'] = 'Bộ lọc';
+$string['firstname'] = 'Họ';
+$string['grade'] = 'Điểm';
+$string['group'] = 'Nhóm';
+$string['incorrectoptions'] = 'Các tùy chọn sai';
+$string['key'] = 'Chú thích';
+$string['landscape'] = 'Landscape';
+$string['max'] = 'max';
+$string['maxposs'] = 'cao nhất';
+$string['maxgrade'] = 'Điểm cao nhất';
+$string['mcq'] = 'Thống kê lượt trả lời MCQ';
+$string['meangrade'] = 'Điểm trung bình';
+$string['mediangrade'] = 'Điểm trung bình';
+$string['never'] = 'Không bao giờ';
+$string['nochoice'] = 'Chưa trả lời';
+$string['option'] = 'Lựa chọn';
+$string['optioncounts'] = 'Số lượng tùy chọn';
+$string['optionschosen'] = 'Các tùy chọn đã chọn';
+$string['orientation'] = 'Orientation';
+$string['partialoption'] = 'Đáp án đúng 1 phần';
+$string['pluginname'] = 'MCQ breakdown';
+$string['portrait'] = 'Portrait';
+$string['printview'] = 'Xuất báo cáo';
+$string['qattempts'] = 'Số lần làm bài';
+$string['questionabbr'] = 'Q';
+$string['quizcloses'] = 'Kết thúc đề thi';
+$string['regstudents'] = 'Học viên đã ghi danh';
+$string['samplesummary'] = 'Tổng quan nhóm';
+$string['sample'] = 'Nhóm';
+$string['scale'] = 'Scale';
+$string['suggestedprint'] = 'Cài đặt được đề xuất';
+$string['surname'] = 'Tên';
+$string['totalchoosing'] = 'Tổng số lựa chọn';
+$string['totalcorrect'] = 'Tổng số câu đúng';
+$string['unanswered'] = 'Không có đáp án';
+$string['userscompleting'] = 'Học viên hoàn thành bài thi';
+$string['usersyettocomplete'] = 'Học viên chưa hoàn thành bài thi';
+$string['warning'] = 'Cảnh báo';
+$string['warningaverage'] = 'Phương pháp chấm điểm được sử dụng là \ "Average \". Tuy nhiên, điểm được lập bảng dựa trên nỗ lực cuối cùng của học sinh và do đó có thể khác với điểm được hiển thị ở những nơi khác.';
+$string['warningnomcq'] = 'Không có câu hỏi trắc nghiệm (không ngẫu nhiên) trong bài kiểm tra này nên không có dữ liệu để hiển thị.';
+$string['warningnotallmcq'] = 'Chỉ có các câu hỏi trắc nghiệm trong bài kiểm tra này được hiển thị bên dưới. Tuy nhiên, điểm được hiển thị bao gồm một hoặc nhiều câu hỏi thuộc các loại khác được sử dụng trong bài kiểm tra này.';
+$string['wholecourse'] = 'Toàn bộ khóa';
+$string['wholegrouping'] = 'Toàn bộ nhóm';
