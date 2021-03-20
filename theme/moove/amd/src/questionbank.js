33c2a2b3922d134c3c364f1276219f09e1a2b2dd
lethanhvuvnr <vu.lethanh@vnresource.net>
2021-03-20 09:43:34 +0700

    Fix câu hỏi & danh mục câu hỏi 2

--- theme/moove/amd/src/questionbank.js
+++ theme/moove/amd/src/questionbank.js
@@ -2,13 +2,15 @@ define(["jquery", "core/config", "core/str", "core/notification"], function($, C
     "use strict";
     var init = function() {
         // question category
-        var form = $('#page-question-category .card .card-body form');
-        var clone = form.clone();
-        form.remove();
-        $('#page-question-category .modal-body').append(clone);
-        $('.add-question-bank').click(function() {
-            $('.createnewquestion .singlebutton button').trigger('click');
-        })
+        if($('div.add-question-bank').length != 0) {
+            var form = $('#page-question-category .card .card-body form');
+            var clone = form.clone();
+            form.remove();
+            $('#page-question-category .modal-body').append(clone);
+            $('.add-question-bank').click(function() {
+                $('.createnewquestion .singlebutton button').trigger('click');
+            })
+        }
     }
     return {
         init: init
