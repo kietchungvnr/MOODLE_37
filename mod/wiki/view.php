ee2565bfbdfff986bbab092d34cda6ae723a7346
lethanhvuvnr <vu.lethanh@vnresource.net>
2021-05-29 11:38:28 +0700

    [Thư viện trực tiếp] Phân quyền wiki

--- mod/wiki/view.php
+++ mod/wiki/view.php
@@ -171,6 +171,32 @@ if ($id) {
     $course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
 
     require_course_login($course, true, $cm);
+
+    // Kiểm tra nếu module đã ẩn thì không đc view
+    if($course->id == 1) {
+        $context = context_module::instance($cm->id);
+        if(is_siteadmin() || user_has_role_assignment($USER->id, 1, $context->id) == true) {
+            // Nothing to do...
+        } else {
+            $module = $DB->get_record('course_modules', array('id' => $id), 'visible', MUST_EXIST);
+            $moduleapproval = $DB->get_record('library_module', array('coursemoduleid' => $id), '*', MUST_EXIST);
+            if($module && $module->visible == 0) {
+                print_error('nopermission', 'local_newsvnr');
+            }
+            switch ($moduleapproval->approval) {
+                case 1:
+                    // Nothing to do...
+                    break;
+                case 0:
+                    if($moduleapproval->userid != $USER->id) {
+                        print_error('nopermission', 'local_newsvnr');
+                    }
+                default:
+                    // Nothing to do...
+                    break;
+            }
+        }
+    }
     /*
      * Case 2:
      *
