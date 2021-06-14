c38dd337ac395f12cc1cb4103aaaec720edfb397
lethanhvuvnr <vu.lethanh@vnresource.net>
2021-06-14 10:18:33 +0700

    [Upbuild] 39_W23.1_2021

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2020061522.22;              // 20200615      = branching date YYYYMMDD - do not modify!
+$version  = 2020061523.23;              // 20200615      = branching date YYYYMMDD - do not modify!
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
-$release  = '3.9+ (Build: 05062021)'; // Human-friendly version name
+$release  = '3.9+ (Build: 12062021)'; // Human-friendly version name
 $branch   = '39';                       // This version's branch.
 $maturity = MATURITY_STABLE;             // This version's maturity level.
-$build_version = '39_W21.1_2021';
+$build_version = '39_W23.1_2021';
